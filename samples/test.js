6ee244630310b88dee4a8fcf20e70b9b882efc40
Damian Cipolat <damian.cipolat@gmail.com>
2019-04-12 21:05:36 -0300

    fix

--- samples/test.js
+++ samples/test.js
@@ -1,4 +1,16 @@
+//Include validator.
 const {intercept,types} = require('../index.js');
+
+//Include data types.
+const {
+  number, 
+  string, 
+  bool,
+  date,
+  array,
+  func,
+  object
+}  = types;
 
 //Sample functions.
 const sum = (a,b)=>{
@@ -14,116 +26,6 @@ const print = (name,surname,age,email)=>{
 }
 
 //Create a decorated function with type validation.
-const sumT = intercept([types.number,types.number])(sum);
+const sumT = intercept([number,number])(sum);
 
 console.log(sumT(1,2));
-
-
-
-
-/*
-
-const {validate} = require('./validator.js');
-const Joi = require('joi');
-
-const lista = ['11',2,'2222'];
-const valid = [Joi.string(),Joi.number(),Joi.string()];
-
-try{
-const tmp = validate(lista,valid);
-console.log(tmp);  
-} catch(err){
-  console.log('--->',err);
-}
-
-
-const prueba = (p1,p2,p3)=>{
-
-  console.log('xxx',p1,p2,p3);
-
-}
-
-function sumar(a,b){
-
-  return a+b;
-
-}
-
-const bar = (...args)=>{
-
-  args.forEach(e=>{
-    console.log('--->',e);
-  });
-
-  prueba(...args);
-
-}
-
-const intercept = (...args) => (fn)=>{
-
-  args.forEach(e=>{
-    console.log('--->',e);
-  });
-
-  return fn(...args);
-
-}
-
-//bar(1,2,3);
-const resu = intercept(1,2)(sumar);
-
-console.log(resu);
-*/
-
-
-/*
-const schema = Joi.object().keys({
-    username: Joi.string().alphanum().min(3).max(30).required(),
-    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
-    access_token: [Joi.string(), Joi.number()],
-    birthyear: Joi.number().integer().min(1900).max(2013),
-    email: Joi.string().email({ minDomainAtoms: 2 })
-});
-
-const result = Joi.validate({ username: 'ab', birthyear: 1994 }, schema);
-*/
-
-
-/*
-const prueba = (p1,p2,p3)=>{
-
-  console.log('xxx',p1,p2,p3);
-
-}
-
-function sumar(a,b){
-
-  return a+b;
-
-}
-
-const bar = (...args)=>{
-
-  args.forEach(e=>{
-    console.log('--->',e);
-  });
-
-  prueba(...args);
-
-}
-
-const intercept = (...args) => (fn)=>{
-
-  args.forEach(e=>{
-    console.log('--->',e);
-  });
-
-  return fn(...args);
-
-}
-
-//bar(1,2,3);
-const resu = intercept(1,2)(sumar);
-
-console.log(resu);
-*/
--- types.js
+++ types.js
@@ -1,6 +1,11 @@
 const Joi = require('joi');
 
 module.exports = {
-	string : Joi.string(),
-	number : Joi.number()
+  string : Joi.string(),
+  number : Joi.number(),
+  bool   : Joi.boolean(),
+  array  : Joi.array(),
+  date   : Joi.date(),
+  func   : Joi.func(),
+  object : Joi.object()
 }
